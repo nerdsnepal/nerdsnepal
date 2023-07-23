66d70d4af73e5debdfe3ae47c2c0f774324b308c
whoamisafal <computerstha12@gmail.com>
2023-07-23 20:27:34 +0545

    Added react-toastify

--- frontend/nerdsnepal-admin/src/services/api.js
+++ frontend/nerdsnepal-admin/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 import { emailValidator, validateUserName, } from "../utils/common"
+import addNotification  from 'react-push-notification'
 
-
-const API_BASE_URL = "http://localhost:8000"
+const API_BASE_URL = "http://192.168.100.13:8000"
 const config = {
     withCredentials:true,
     headers:{
@@ -19,6 +19,13 @@ const login =async (email_or_username,password)=>{
         password:password,
         username:validateUserName(email_or_username)?email_or_username:""
     },config)
+    addNotification({
+        title:"Success",
+        silent:false,
+        subtitle:"This is login",
+        theme: 'darkblue',
+        native: true
+    })
     return result.data
   } catch (error) {
     return Promise.reject({error})
@@ -39,6 +46,7 @@ const getAllCategory = async()=>{
     try {
     
         let result = await axios.get(`${API_BASE_URL}/admin/category`,{withCredentials:true})
+        
         return result.data
       } catch (error) {
         return Promise.reject({error})
